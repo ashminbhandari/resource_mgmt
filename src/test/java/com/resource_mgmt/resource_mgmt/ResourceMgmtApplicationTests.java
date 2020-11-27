8dd3c7a8de0642e04737f98614813e391231afc0
ashminbhandari <abhandar@ramapo.edu>
2020-11-27 16:43:24 -0500

    ##spring initializr

---
+++ src/test/java/com/resource_mgmt/resource_mgmt/ResourceMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resource_mgmt.resource_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
