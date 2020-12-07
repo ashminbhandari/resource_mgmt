3d0ba3f596c2266ccab5eee7ad54e912f83cc7c0
ashminbhandari <abhandar@ramapo.edu>
2020-12-07 09:13:52 -0500

    #fix proj controller/other errors. wrong package export/import

--- src/main/java/com/resource_mgmt/resource_mgmt/service/ProjectColumnServiceImpl.java
+++ src/main/java/com/resource_mgmt/resource_mgmt/service/ProjectColumnServiceImpl.java
@@ -5,7 +5,6 @@ import com.resource_mgmt.resource_mgmt.repository.ProjectColumnRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
